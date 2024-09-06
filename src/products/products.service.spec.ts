003094eeb869c0ad9c006c3e1e054576dd9664c5
Marcos Parreiras <marcosalp1998@gmail.com>
2024-09-05 21:30:27 -0300

    ci: add github workflow

--- src/products/products.service.spec.ts
+++ src/products/products.service.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
+import { getModelToken } from '@nestjs/sequelize';
+import { Product } from './entities/product.entity';
 
 describe('ProductsService', () => {
   let service: ProductsService;
 
+  const mockProductModel = {
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    destroy: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductsService],
+      providers: [
+        ProductsService,
+        {
+          provide: getModelToken(Product),
+          useValue: mockProductModel,
+        },
+      ],
     }).compile();
 
     service = module.get<ProductsService>(ProductsService);
